5eebe796b97ff6c3a0396c99d7b37f482bb95f14
Swagato Bhaskar <bhaskarswagato@gmail.com>
2021-08-25 22:59:31 +0530

    initial files with tailwind css

---
+++ components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="">
+            <p className="">Nous Mangeons</p>
+        </div>
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+//import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    );
 }
 
 export default MyApp
